7c61125754696929516f16f3f19d923a3c865278
lorena-work <145394254+lorena-work@users.noreply.github.com>
2024-01-16 13:25:54 -0800

    Complete removeFromArray assignment and test for functionality

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((element) => {
+        if (!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+    //pushes each element included in "array" into an array named "newArray" if the element is NOT included in args
+    return newArray;
+}
 
-};
+//console.log(removeFromArray([1,2,3,4],3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
