2ceb9e9f4b9e441c88414c2db5e58406592857a0
lorena-work <145394254+lorena-work@users.noreply.github.com>
2024-01-16 14:31:02 -0800

    Complete and test sumAll function assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
 
+const sumAll = function(intOne,intTwo) {
+    let sum = 0;
+    //works with larger number first
+    if(intOne>intTwo){
+        const placeholder= intOne;
+        intOne = intTwo;
+        intTwo = placeholder;
+    }
+   //returns ERROR with negative numbers
+    if(intOne< 0 || intTwo <0){
+        return "ERROR";
+    }
+    //returns ERROR with non-number parameters
+    if(!Number.isInteger(intOne) || !Number.isInteger(intTwo)){
+        return "ERROR";
+    }
+    //start at intOne then add every number up to intTwo
+    //return sum of all numbers
+    for(let i = intOne; i <= intTwo; i++){
+        sum += i;
+    }
+    
+return sum;
 };
+//console.log(sumAll(1,4))
 
 // Do not edit below this line
 module.exports = sumAll;
