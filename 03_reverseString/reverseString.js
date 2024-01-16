fb423e94365ec18f3c5bce97f04ed9af3fb798dd
lorena-work <145394254+lorena-work@users.noreply.github.com>
2024-01-16 11:48:29 -0800

    Try different method to get the same result for the reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,20 @@
 const reverseString = function(word) {
    //let reversed = word.split('').reverse().join('');
    //return reversed;
+   let reversed = "";
+   for (let i = word.length- 1; i >= 0; i--){
+      reversed+= word[i];
 
-   
+      //for i = hello, i = 4 & reversed = ""
+      //"" + word[4]= o
+      //o + word[3] = ol
+      //ol + word[2] = oll
+      //oll + word[1] = olle
+      //olle + word[0]= olleh
+   }
+   return reversed;
 }
+//console.log(reverseString("hello"))
+
 // Do not edit below this line
 module.exports = reverseString;
